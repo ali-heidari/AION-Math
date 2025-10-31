bdeb3f900f13448bdb1ce28c6e944b411306bded
ali-heidari <ali-heidari@outlook.com>
2025-10-31 11:24:11 +0330

    Fix ema

--- src/continuous_math/impl_average.rs
+++ src/continuous_math/impl_average.rs
@@ -41,47 +41,71 @@ impl ContinuousMath {
         (avg, trend)
     }
 
-    pub fn sma(&mut self, label: u32, count: usize, new_value: f32) -> f32 {
+    pub fn sma(&mut self, label: u32, count: usize, new_value: f32) -> Option<f32> {
         let frame: &mut Frame = self.get_frame(label);
 
         frame.values.push_back(new_value);
 
         if frame.values.len() < count {
-            return -1.0;
+            return None;
         }
 
         if frame.values.len() == count {
             let values = frame.values.as_slices();
             frame.calculated_value = Math::sma(values.0);
-            return frame.calculated_value;
+            return Some(frame.calculated_value);
         }
 
         let first_value = frame.values.pop_front().unwrap();
         frame.count = count;
         frame.calculated_value += (new_value - first_value) / frame.count as f32;
 
-        frame.calculated_value
+        Some(frame.calculated_value)
     }
 
-    pub fn ema(&mut self, label: u32, count: usize, new_value: f32) -> f32 {
+    pub fn ema(&mut self, label: u32, count: usize, new_value: f32) -> Option<f32> {
         let frame: &mut Frame = self.get_frame(label);
 
         frame.values.push_back(new_value);
 
         if frame.values.len() < count {
-            return -1.0;
+            return None;
         }
 
         if frame.values.len() == count {
             let values = frame.values.as_slices();
             frame.calculated_value = Math::sma(values.0);
-            return frame.calculated_value;
+            return Some(frame.calculated_value);
         }
 
         let smoothing_factor: f32 = 2.0 / (count + 1) as f32;
-        frame.calculated_value += smoothing_factor * (new_value - frame.calculated_value);
+        frame.calculated_value =
+            (new_value * smoothing_factor) + (frame.calculated_value * (1.0 - smoothing_factor));
 
-        frame.calculated_value
+        Some(frame.calculated_value)
+    }
+
+    pub fn macd(
+        &mut self,
+        label: u32,
+        fast_ema: usize,
+        slow_ema: usize,
+        signal_line_ema: usize,
+        new_value: f32,
+    ) -> Option<(f32, f32)> {
+        let fast_ema = self.ema(label, fast_ema, new_value);
+        let slow_ema = self.ema(label + 1, slow_ema, new_value);
+
+        slow_ema?;
+
+        let macd_line = fast_ema.unwrap() - slow_ema.unwrap();
+
+        let signal_line_ema = self.ema(label + 2, signal_line_ema, macd_line);
+
+        signal_line_ema?;
+
+        println!("asds {:?}", (fast_ema, slow_ema, signal_line_ema));
+        Some((macd_line, signal_line_ema.unwrap()))
     }
 }
 
@@ -119,47 +143,111 @@ mod tests {
     fn sma() {
         let mut continuous_math: ContinuousMath = ContinuousMath::new();
         let mut result = continuous_math.sma(1, 5, 10.0);
-        assert_eq!(result, -1.0);
+        assert_eq!(result, None);
         result = continuous_math.sma(1, 5, 11.0);
-        assert_eq!(result, -1.0);
+        assert_eq!(result, None);
         result = continuous_math.sma(1, 5, 12.0);
-        assert_eq!(result, -1.0);
+        assert_eq!(result, None);
         result = continuous_math.sma(1, 5, 11.0);
-        assert_eq!(result, -1.0);
+        assert_eq!(result, None);
         result = continuous_math.sma(1, 5, 14.0);
-        assert_eq!(result, 11.6);
+        assert_eq!(result, Some(11.6));
         result = continuous_math.sma(1, 5, 15.0);
-        assert_eq!(result, 12.6);
+        assert_eq!(result, Some(12.6));
         result = continuous_math.sma(1, 5, 13.0);
-        assert_eq!(result, 13.0);
+        assert_eq!(result, Some(13.0));
     }
 
     #[test]
     fn ema() {
         let mut continuous_math: ContinuousMath = ContinuousMath::new();
         let mut result = continuous_math.ema(1, 10, 22.0);
-        assert_eq!(result, -1.0);
+        assert_eq!(result, None);
         result = continuous_math.ema(1, 10, 24.0);
-        assert_eq!(result, -1.0);
+        assert_eq!(result, None);
         result = continuous_math.ema(1, 10, 26.0);
-        assert_eq!(result, -1.0);
+        assert_eq!(result, None);
         result = continuous_math.ema(1, 10, 25.0);
-        assert_eq!(result, -1.0);
+        assert_eq!(result, None);
         result = continuous_math.ema(1, 10, 28.0);
-        assert_eq!(result, -1.0);
+        assert_eq!(result, None);
         result = continuous_math.ema(1, 10, 27.0);
-        assert_eq!(result, -1.0);
+        assert_eq!(result, None);
         result = continuous_math.ema(1, 10, 29.0);
-        assert_eq!(result, -1.0);
+        assert_eq!(result, None);
         result = continuous_math.ema(1, 10, 30.0);
-        assert_eq!(result, -1.0);
+        assert_eq!(result, None);
         result = continuous_math.ema(1, 10, 29.0);
-        assert_eq!(result, -1.0);
+        assert_eq!(result, None);
         result = continuous_math.ema(1, 10, 32.0);
-        assert_eq!(result, 27.2);
+        assert_eq!(result, Some(27.2));
         result = continuous_math.ema(1, 10, 35.0);
-        assert_eq!(result, 28.618183);
+        assert_eq!(result, Some(28.618181));
         result = continuous_math.ema(1, 10, 33.0);
-        assert_eq!(result, 29.414877);
+        assert_eq!(result, Some(29.414875));
+    }
+
+    #[test]
+    fn macd() {
+        let mut continuous_math: ContinuousMath = ContinuousMath::new();
+        let mut result = continuous_math.macd(1, 12, 26, 9, 55.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 57.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 58.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 60.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 62.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 61.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 63.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 64.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 65.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 67.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 66.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 68.0);
+        result = continuous_math.macd(1, 12, 26, 9, 69.0);
+        result = continuous_math.macd(1, 12, 26, 9, 70.0);
+        result = continuous_math.macd(1, 12, 26, 9, 72.0);
+        result = continuous_math.macd(1, 12, 26, 9, 71.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 73.0);
+        result = continuous_math.macd(1, 12, 26, 9, 72.0);
+        result = continuous_math.macd(1, 12, 26, 9, 74.0);
+        result = continuous_math.macd(1, 12, 26, 9, 75.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 76.0);
+        result = continuous_math.macd(1, 12, 26, 9, 78.0);
+        result = continuous_math.macd(1, 12, 26, 9, 79.0);
+        result = continuous_math.macd(1, 12, 26, 9, 80.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 82.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 81.0);
+        result = continuous_math.macd(1, 12, 26, 9, 80.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 78.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 77.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 76.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 75.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 77.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 76.0);
+        assert_eq!(result, None);
+        result = continuous_math.macd(1, 12, 26, 9, 79.0);
+        assert_eq!(result, Some((4.05381, 5.473577)));
+        result = continuous_math.macd(1, 12, 26, 9, 80.0);
+        assert_eq!(result, Some((4.000931, 5.179048)));
     }
 }
