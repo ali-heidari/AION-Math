69ebf261ef1ed64ffeeede5d0b78268dce88431e
ali-heidari <ali-heidari@outlook.com>
2025-10-31 07:21:46 +0330

    Add sma to the Math

--- src/math/impl_average.rs
+++ src/math/impl_average.rs
@@ -4,4 +4,8 @@ impl Math {
     pub fn average(&mut self, values: &[f32]) -> f32 {
         values.iter().sum::<f32>() / values.len() as f32
     }
+
+    pub fn sma(&self, values: &[f32]) {
+        self.average(values)
+    }
 }
